1723ef4498585d2d0de9ff6b4ecc5308f5a67992
Manuel <manuel@masiasweb.com>
2021-12-26 12:03:37 +0100

    8 personajes, ajuste para evitar que cuando hayan dos NPCs compartiendo espacio se impriman dos veces. (No me termina de convencer esta forma de hacerlo, a ver si se me ocurre algo más elegante).

--- npcBloqueV2.java
+++ npcBloqueV2.java
@@ -2,31 +2,36 @@ import java.util.Scanner;
 public class npcBloqueV2{
 
 	public static void main(String[] args){
-		int npc[][] = { 
-			{2,2},		// Coordenadas X,Y del 1º NPC 
+		int npc[][] = {
+			{2,2},		// Coordenadas X,Y del 1º NPC
 			{5,5}, 		// Coordenadas X,Y del 2º NPC
-			{6,9}		// Coordenadas X,Y del 3º NPC
+			{6,9},		// Coordenadas X,Y del 3º NPC
+			{8,8},{8,9},{8,10},{8,11},{8,12}
 		};
-		
+
 		int elMundo[][] = {
-			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+			{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}
 		};
-				
+
 		boolean hayNPC=false;
 
 		Scanner entrada = new Scanner(System.in);
 		String inputUsuario;
-		
-		for (int minutos=0; minutos<20; minutos++){
+
+		for (int minutos=0; minutos<60; minutos++){
 			// Codigo experimental de limpieza de pantalla (podria no funcionar en todos los terminales)
 				System.out.print("\033[H\033[2J");
 				System.out.flush();
@@ -35,18 +40,19 @@ public class npcBloqueV2{
 			// INICIO Impresion del mapa
 			for (int i=0; i<elMundo.length; i++) {
 				for (int j=0; j<elMundo[0].length; j++) {
-					
-					// Analizo si en la posición a imprimir hay un NPC. 
-					// Si lo hay, lo imprimo y me guardo una bandera (hayNPC) 
+
+					// Analizo si en la posición a imprimir hay un NPC.
+					// Si lo hay, lo imprimo y me guardo una bandera (hayNPC)
 					// para no imprimir lo que va allí
 					hayNPC=false;
 					for (int unNPC=0;unNPC<npc.length;unNPC++) {
 						if(npc[unNPC][0]==i && npc[unNPC][1]==j){
 							System.out.print("^"+unNPC+"^");
 							hayNPC=true;
+							break;	// Si imprimo uno, ya salgo (para no imprimir)
 						}
 					}
-					
+
 					// Verifico la bandera anteriormente configurada, para ver
 					// si hay que imprimir lo que toca o no
 					if(!hayNPC) { System.out.print(" ' "); }
@@ -57,19 +63,19 @@ public class npcBloqueV2{
 
 			// INICIO gestión del movimiento de los NPC
 			for (int unNPC=0; unNPC<npc.length; unNPC++){
-				
+
 				int dado=(int)(Math.random()*4)+1;
-				
+
 				if(dado==1){npc[unNPC][0]=npc[unNPC][0]+1;} else
 				if(dado==2){npc[unNPC][0]=npc[unNPC][0]-1;} else
 				if(dado==3){npc[unNPC][1]=npc[unNPC][1]+1;} else
-				if(dado==4){npc[unNPC][1]=npc[unNPC][1]-1;} 
+				if(dado==4){npc[unNPC][1]=npc[unNPC][1]-1;}
 			}
 			// FIN gestión del movimiento de los NPC
 
 			inputUsuario=entrada.nextLine(); // Pausa para reflexionar... ;)
 		}
-		
+
 
 	}
 }
